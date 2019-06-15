3b22ffa460243830d3a0bc717018c7279b9efd08
ファム ヴァンフン <vanhung.pham@mixi.co.jp>
2019-06-15 18:14:43 +0900

    fix spec

--- spec/switch_point_spec.rb
+++ spec/switch_point_spec.rb
@@ -2,16 +2,12 @@
 
 RSpec.describe SwitchPoint do
   describe '.master_all!' do
-    after do
-      SwitchPoint.slave_all!
-    end
-
     it 'changes connection globally' do
-      expect(Book).to connect_to('main_slave.sqlite3')
-      expect(Book3).to connect_to('main2_slave.sqlite3')
-      expect(Comment).to connect_to('comment_slave.sqlite3')
+      expect(Book).to connect_to('main_master.sqlite3')
+      expect(Book3).to connect_to('main2_master.sqlite3')
+      expect(Comment).to connect_to('comment_master.sqlite3')
       expect(User).to connect_to('user.sqlite3')
-      expect(BigData).to connect_to('main_slave_special.sqlite3')
+      expect(BigData).to connect_to('main_master.sqlite3')
       SwitchPoint.master_all!
       expect(Book).to connect_to('main_master.sqlite3')
       expect(Book3).to connect_to('main2_master.sqlite3')
@@ -46,14 +42,9 @@ RSpec.describe SwitchPoint do
   end
 
   describe '.master!' do
-    after do
-      SwitchPoint.slave!(:main)
-    end
-
     it 'changes connection globally' do
-      expect(Book).to connect_to('main_slave.sqlite3')
-      expect(Publisher).to connect_to('main_slave.sqlite3')
-      SwitchPoint.master!(:main)
+      expect(Book).to connect_to('main_master.sqlite3')
+      expect(Publisher).to connect_to('main_master.sqlite3')
       expect(Book).to connect_to('main_master.sqlite3')
       expect(Publisher).to connect_to('main_master.sqlite3')
     end
@@ -71,7 +62,7 @@ RSpec.describe SwitchPoint do
           SwitchPoint.slave!(:main)
           expect(Book).to connect_to('main_slave.sqlite3')
         end
-        expect(Book).to connect_to('main_slave.sqlite3')
+        expect(Book).to connect_to('main_master.sqlite3')
         Book.with_master do
           expect(Book).to connect_to('main_master.sqlite3')
         end
@@ -90,11 +81,11 @@ RSpec.describe SwitchPoint do
       SwitchPoint.with_master(:main, :nanika1) do
         expect(Book).to connect_to('main_master.sqlite3')
         expect(Publisher).to connect_to('main_master.sqlite3')
-        expect(Nanika1).to connect_to('default.sqlite3')
+        expect(Nanika1).to connect_to('main_master.sqlite3')
       end
-      expect(Book).to connect_to('main_slave.sqlite3')
-      expect(Publisher).to connect_to('main_slave.sqlite3')
-      expect(Nanika1).to connect_to('main_slave.sqlite3')
+      expect(Book).to connect_to('main_master.sqlite3')
+      expect(Publisher).to connect_to('main_master.sqlite3')
+      expect(Nanika1).to connect_to('main_master.sqlite3')
     end
 
     context 'with unknown name' do
@@ -106,8 +97,8 @@ RSpec.describe SwitchPoint do
 
   describe '.with_master_all' do
     it 'changes all connections' do
-      expect(Book).to connect_to('main_slave.sqlite3')
-      expect(Comment).to connect_to('comment_slave.sqlite3')
+      expect(Book).to connect_to('main_master.sqlite3')
+      expect(Comment).to connect_to('comment_master.sqlite3')
       SwitchPoint.with_master_all do
         expect(Book).to connect_to('main_master.sqlite3')
         expect(Comment).to connect_to('comment_master.sqlite3')
